df63e35e76113b9f107e98a456f01f3c9c76d33b
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2021-02-09 13:44:15 -0300

    working in new brach

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 class People{
 
-    constructor(type){
-        this._type = type
+    constructor(){
+        this._type = 'Person'
         this._date = new Date().getTime()
     }
-    static createMedician(id,name,type){
-        return new People('Medico')
+    
+    static createEnginer(id,name,type){
+        class Enginer extends People{
+            constructor(name, id){
+                super(id, name, type)
+                this._name = name
+                this._id = id
+                this._type = 'Enginner'
+            }
+        }
+
+        return new Enginer(id,name,type);
+    }
+
+    static createStudent(id, name, course,type){
+        class Student extends People{
+            constructor(id, name, course){
+                super(id,name,type)
+                this._id = id 
+                this._name = name 
+                this._course = course 
+                this._type = 'Student'
+            }
+
+        }
+        return new Student(id, name, course);
     }
 }
 
-const novaPessoa = People.createMedician('1','Paulo')
+const novaPessoa1 = People.createStudent('33','Rafael','Medicina')
+const novaPessoa2 = People.createEnginer('Andressa','2')
 
-console.log(novaPessoa)
+console.log(novaPessoa1)
+console.log(novaPessoa2)
 
 
